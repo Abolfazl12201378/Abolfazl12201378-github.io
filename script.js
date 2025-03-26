15481bb6853b9ca04210cd5b1edea882999262d8
Abolfazl Farrokhi <166596447+Abolfazl12201378@users.noreply.github.com>
2025-03-26 23:26:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var w = c.width = window.innerWidth,
     opts = {
         get strings() {
             const sentences = [
-                ['HAPPY', 'BIRTHDAY! , Sarina...!'],
+                ['HAPPY', 'BIRTHDAY! , Zahra jooooooon ...!'],
             ];
 
             return sentences[sentenceIndex++ % sentences.length];
